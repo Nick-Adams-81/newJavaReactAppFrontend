364d8c9c83c5afa0faded383493e1a0ce56d9978
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-12 10:01:45 -0700

    updated delete method

--- src/Components/CustomHooks/DELETE.js
+++ src/Components/CustomHooks/DELETE.js
@@ -4,12 +4,10 @@ const deleteUser = (id) => {
     fetch(`http://localhost:8080/delete/${id}`, {
         method: "DELETE",
         headers: {"Content-type" : "application/json"},
+        credentials: "include"
     })
     .then(() => console.log("user deleted successfully!"))
     .catch((err) => console.log(err))
 
-    // const removeUser = (id) => {
-    //     setData(current => current.filter(user => {return user.id !== id}));
-    //   }
 }
 export default deleteUser;
