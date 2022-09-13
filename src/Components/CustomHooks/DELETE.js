40f1e099f458af9841a85b63115caa0025956d0f
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-13 11:56:19 -0700

    added completed delete http request

--- src/Components/CustomHooks/DELETE.js
+++ src/Components/CustomHooks/DELETE.js
@@ -1,7 +1,7 @@
 
 
 const deleteUser = (id) => {
-    fetch(`http://localhost:8080/delete/${id}`, {
+    fetch(`http://localhost:8080/users/delete/${id}`, {
         method: "DELETE"
     })
     .then(() => console.log("user deleted successfully!"))
